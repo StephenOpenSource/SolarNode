f2c6a4d444d86b334874ca20edd9918cbea34a43
stshigame <stshigame@users.noreply.github.com>
2019-05-23 10:32:31 -0700

    Menu container stub
    Basic stub for a pop out menu including collapse logic

---
+++ client/src/modules/Menu.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './Menu.css';
+
+var expandArr = '\u21c9';
+var collapseArr  = '\u21c7';
+
+class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapsed: false,
+            arrow: expandArr
+        }
+
+        this.onCollapse = this.onCollapse.bind(this);
+    }
+
+    componentDidMount() {
+        if(this.state.collapsed) {
+            document.getElementById('menu-container').classList.add('collapse');
+            this.setState({
+                arrow: expandArr
+            })
+        } else {
+            document.getElementById('menu-container').classList.remove('collapse');
+            this.setState({
+                arrow: collapseArr
+            })
+        }
+    }
+
+    onCollapse() {
+        document.getElementById("menu-container").classList.toggle("collapse");
+        this.setState({
+            collapsed: !this.state.collapsed,
+            arrow: this.state.collapsed ? collapseArr : expandArr
+        });
+    }
+
+    render() {
+        return (
+            <div className="flex-container">
+                <div id="menu-container" className="menu-container" onClick={this.onCollapse} onMouseEnter={() => {console.log("Hover menu")}}>
+                    This is a {this.props.name} Menu Component
+                </div>
+                <div>
+                    <button className="collapsible menu-button" onClick={this.onCollapse}>{this.state.arrow}</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Menu;
