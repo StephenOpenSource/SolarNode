44f274b0d13a235ef1aa8629db27aa162dd67a68
stshigame <stshigame@users.noreply.github.com>
2019-05-23 10:31:42 -0700

    Adding Collapsible logic
    ability to collapse and expand the React info portion, including CSS for using a styled button

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './modules/Menu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <button className="collapsible react-button" onClick={() => {
+          document.getElementById("React-header").classList.toggle("collapse");
+        }}
+        >React Info</button>
+      <header id="React-header" className="App-header collapse">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +23,9 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
+      <Menu name="New" />
     </div>
   );
 }
